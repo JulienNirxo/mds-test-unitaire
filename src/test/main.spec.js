3d9dce4de14bffa269cb93d218f7de521b88292e
JulienNirxo <millaud.julien@gmail.com>
2024-01-18 10:25:51 +0100

    [FEAT] v1 test

--- src/test/main.spec.js
+++ src/test/main.spec.js
@@ -1,5 +1,8 @@
 const {main} = require('../main')
 it('should retrun world of param is hello', () => {
-    const result = main("hello")
     expect(main("hello")).toBe("world")
+})
+
+it('should retrun nop of param is not hello', () => {
+    expect(main("not hello")).toBe("nop")
 })
